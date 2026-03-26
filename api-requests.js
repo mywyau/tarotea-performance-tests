32d7b14cee2390c91e949217e39fa905cc83ba2b
michael-hmrc <47248104+michael-hmrc@users.noreply.github.com>
2026-03-26 00:05:27 +0000

    wip

--- api-requests.js
+++ api-requests.js
@@ -1,13 +1,12 @@
-export function levelQuizRequest() {
-  http.get(
+export function levelQuizRequest(headers) {
+  return http.get(
     `${BASE_URL}/api/vocab-quiz/${LEVEL_SLUG}`,
     { headers }
   );
 }
 
-
-export function finalizeLevelQuiz() {
-  http.post(
+export function finalizeLevelQuiz(headers, payload) {
+  return http.post(
     `${BASE_URL}/api/quiz/grind/finalize`,
     JSON.stringify(payload),
     { headers }
